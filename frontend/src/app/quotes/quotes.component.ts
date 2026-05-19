2dfda37ee0cf02368c196f5b54570d667e5f6218
dbozman <dane@danebozman.com>
2026-05-19 15:48:29 -0700

    Refactor quote handling and improve URL management

--- frontend/src/app/quotes/quotes.component.ts
+++ frontend/src/app/quotes/quotes.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-
-import { AddQuoteComponent } from './add-quote/add-quote.component';
 import { QuoteListComponent } from './quote-list/quote-list.component';
 
 @Component({
   selector: 'app-quotes',
   standalone: true,
-  imports: [QuoteListComponent, AddQuoteComponent],
+  imports: [QuoteListComponent],
   templateUrl: './quotes.component.html',
   styleUrl: './quotes.component.scss',
 })
